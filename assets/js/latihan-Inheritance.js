09ad4feed032b5b3a5aec5927fee387d948c0380
Yoyourstar <diohidian@gmail.com>
2024-01-19 18:48:57 +0700

    penambahan latihan-inheritance

--- assets/js/latihan-Inheritance.js
+++ assets/js/latihan-Inheritance.js
@@ -4,9 +4,15 @@ class Kendaraan {
         this.merk = merk
         this.tahunProduksi = tahunProduksi
     }
+    // 3. Buat Function hitung usia kendaraan
+    hitungUsia() {
+        const usia = new Date().getFullYear() - this.tahunProduksi;
+        console.log(`Usia kendaraan ${this.merk} adalah ${usia} tahun.`);
+    }
 
 }
 
+// 2. Buat class mobil dan motor
 class Mobil extends Kendaraan {
     constructor(merk, tahunProduksi) {
         super(merk, tahunProduksi)
@@ -18,3 +24,11 @@ class Motor extends Kendaraan {
         super(merk, tahunProduksi)
     }
 }
+
+const avanza = new Mobil("Toyota", 2010)
+console.log(avanza);
+avanza.hitungUsia()
+
+const beat = new Motor("Honda", 2015)
+console.log(beat);
+beat.hitungUsia()
